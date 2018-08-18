5331678f40136b99fc8a82797d90f6ac13851b93
Mitch <Mitchelltmarino@gmail.com>
2018-08-18 14:04:19 -0400

    Basic

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+import os 
 app = Flask(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 @app.route("/")
 def hello():
@@ -7,4 +10,4 @@ def hello():
 
 @app.route("/profile")
 def profile():
-    return "Hello this is your profile!"
+    return render_template('Buttons.html')
